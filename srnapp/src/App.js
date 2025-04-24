2e1d02bdcdece1dbacfad71c97a00e74117ffbbd
sharanpatel19 <sharanpatel1946@gmail.com>
2025-04-24 10:06:38 +0530

    done

---
+++ srnapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header'
+
+function App() {
+  return (
+    <div>
+      <h1>HELLO!</h1>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ srnapp/src/Components/Header.js
@@ -0,0 +1,9 @@
+import './Header.css'
+function Header(){
+return(
+    <div>
+        <h1>hello sharan here!!</h1>
+    </div>
+)
+}
+export default Header;
